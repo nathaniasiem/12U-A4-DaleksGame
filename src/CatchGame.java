a0bf28b18c638526e3251ae86df43ae7c2864595
simon7323 <simon7323@HRH-IC0027294.wrdsb.ca>
2017-10-26 13:19:40 -0400

    26/10/2017- catch game method modified

--- src/CatchGame.java
+++ src/CatchGame.java
@@ -21,14 +21,24 @@ public class CatchGame {
      * (create people, set positions, etc.)
      */
     public CatchGame() {
-        //create  the 12x12 board
+        //create the 12x12 board
         Board b = new Board(12,12);
-        this.doctor=new Doctor((int)*Math.random(12),(int)*Math.random(12));
-        Dalek dk1=new Dalek();
-        Dalek dk2 = new Dalek();
-        Dalek dk3 = new Dalek();
-
-
+        //create positions
+        int bRow=(int)Math.random()*12;
+        int bCol = (int)Math.random()*12;
+        
+        //create people
+        Doctor doctor= new Doctor(bRow,bCol);
+        Dalek dk1 = new Dalek(bRow,bCol);
+        Dalek dk2 = new Dalek(bRow,bCol);
+        Dalek dk3 = new Dalek(bRow,bCol);
+        
+        //places characters onto the board
+        b.putPeg(Color.BLACK, dk1.getRow(),dk1.getCol());
+        b.putPeg(Color.BLACK, dk2.getRow(),dk2.getCol());
+        b.putPeg(Color.BLACK, dk3.getRow(),dk3.getCol());
+        
+        
     }
 
     /**
@@ -36,5 +46,8 @@ public class CatchGame {
      * selects a square, when the Daleks move, when the game is won/lost.
      */
     public void playGame() {
+        
+            
+        }
     }
 }
